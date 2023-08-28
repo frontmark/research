7d78edf28b586137b3bc3c8ae0686d86578edb68
favmd <m.dietl@frontmark.de>
2023-08-28 16:40:37 +0200

    Update gen_img_from_lay.py

--- BlockVisualisation/sample/gen_img_from_lay.py
+++ BlockVisualisation/sample/gen_img_from_lay.py
@@ -1,14 +1,10 @@
-from collections.abc import Iterable
-
 import matplotlib as mpl
 import matplotlib.collections  # call as mpl.collections
 import matplotlib.pyplot as plt
-import numpy as np
 import networkx as nx
 
-
-INFILENAME = "2011_05_19.gml"
-OUTFILENAME = "2011_05_19_out.txt"
+INFILENAME = "2013_12_12.gml"
+OUTFILENAME = "2013_12_12_out.txt"
 
 
 edge_pos = []
@@ -70,5 +66,5 @@ edge_collection = mpl.collections.LineCollection(
 
 ax.add_collection(edge_collection)
 plt.axis("off")
-plt.savefig("2011_05_19_pic.png", dpi=1000)
+plt.savefig("2013_12_12_pic.png", dpi=1000)
 plt.show()
